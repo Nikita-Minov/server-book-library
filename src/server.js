cb0b0c478aca8164e6061384a0d66bb89fc7583b
Nikita-Minov <nikitachryackhov555@gmail.com>
2021-08-12 12:34:52 +0300

    fdfd

--- src/server.js
+++ src/server.js
@@ -59,10 +59,10 @@ passport.deserializeUser(function (userId, done) {
 
 const app = express();
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  res.set("Access-Control-Allow-Origin", '*');
+  res.set("Access-Control-Allow-Credentials", true);
+  res.set('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.set("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next();
 });
 app.use(cookieParser());
