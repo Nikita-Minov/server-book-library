70c4f77f41d2a91299f8e3e55158926a184f52c0
Nikita-Minov <nikitachryackhov555@gmail.com>
2021-08-14 15:32:27 +0300

    fdfd

--- src/server.js
+++ src/server.js
@@ -62,7 +62,7 @@ app.use(cors({
   origin: 'https://still-waters-66948.herokuapp.com',
   credentials: true
 }));
-app.use(cookieParser());
+app.use(cookieParser('keyboard cat'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(session({secret: 'keyboard cat', resave: false, saveUninitialized: false}));
