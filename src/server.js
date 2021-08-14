4b8944468c614a0128b30c6838d72e821c6f6007
Nikita-Minov <nikitachryackhov555@gmail.com>
2021-08-14 16:47:39 +0300

    dfdf

--- src/server.js
+++ src/server.js
@@ -63,7 +63,7 @@ app.use(cors({
   origin: 'https://still-waters-66948.herokuapp.com',
   credentials: true
 }));
-app.use(cookieParser('keyboard cat'));
+app.use(cookieParser('express secret'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(session({
